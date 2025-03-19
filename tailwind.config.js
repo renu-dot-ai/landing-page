ccc5044b82e12ceb83473085dfbf7c9357ed8b45
Sathya Vittal <sathyavijayan@gmail.com>
2025-03-19 19:08:46 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Space Grotesk', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
